192893d69fda69a4fb6375028466d04383c29e4b
атурин ван <baturin.i@asbc.ru>
2022-08-30 18:11:24 +0300

    add style for main container

--- app/components/ui/Layout/Layout.tsx
+++ app/components/ui/Layout/Layout.tsx
@@ -5,13 +5,15 @@ import Header from '@/ui/Layout/Header/Header'
 import Meta from '@/utils/Meta/Meta'
 import { MetaProps } from '@/utils/Meta/meta.interface'
 
+import styles from './Layout.module.scss'
+
 const Layout: FC<PropsWithChildren<MetaProps>> = ({ children, ...meta }) => {
 	return (
 		<>
 			<Meta {...meta} />
 			<div>
 				<Header />
-				<main>{children}</main>
+				<main className={styles.main}>{children}</main>
 			</div>
 		</>
 	)
