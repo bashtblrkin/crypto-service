95c7436c1fa555cae33d7618ea4b3fff878a9139
атурин ван <baturin.i@asbc.ru>
2022-08-30 18:14:21 +0300

    create base login form only with logic

--- app/components/ui/Layout/Header/LoginForm/LoginForm.tsx
+++ app/components/ui/Layout/Header/LoginForm/LoginForm.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+import { AuthField } from '@/ui/Layout/Header/LoginForm/loginForm.interface'
+
+import { useAuth } from '@/hooks/useAuth'
+import { useOutside } from '@/hooks/useOutside'
+
+import styles from './LoginForm.module.scss'
 
 const LoginForm = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+	const { ref, setIsShow, isShow } = useOutside(false)
 
-export default LoginForm;
+	const [type, setType] = useState<'login' | 'register'>('login')
+
+	const {
+		register,
+		formState: { errors },
+		handleSubmit
+	} = useForm<AuthField>({
+		mode: 'onChange'
+	})
+
+	const { setUser } = useAuth()
+
+	const onSubmit: SubmitHandler<AuthField> = (data) => {
+		if (type === 'login')
+			setUser({
+				id: 1,
+				name: 'Ivan',
+				avatarPath: '/avatar.png',
+				email: 'mymail@mail.ru'
+			})
+	}
+
+	return <div>LoginForm</div>
+}
+
+export default LoginForm
