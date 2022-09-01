10eda4031bd9b940a5c7fda7dda14dfa4d2a1ed4
атурин ван <baturin.i@asbc.ru>
2022-09-01 17:28:05 +0300

    add animation

--- app/components/ui/Layout/Header/DropDownWrapper/DropDownWrapper.tsx
+++ app/components/ui/Layout/Header/DropDownWrapper/DropDownWrapper.tsx
@@ -1,25 +1,38 @@
+import { motion } from 'framer-motion'
 import React, { FC, PropsWithChildren } from 'react'
 
 import { DropDownWrapperProps } from '@/ui/Layout/Header/DropDownWrapper/dropDownWrapper.interface'
 
 import { useOutside } from '@/hooks/useOutside'
 
+import { DROP_DOWN_LEFT_TO_RIGHT_ANIMATION } from '@/utils/animation/dropdown'
+
 import styles from './DropDownWrapper.module.scss'
 
 const DropDownWrapper: FC<PropsWithChildren<DropDownWrapperProps>> = ({
 	children,
-    clickableElement
+	clickableElement
 }) => {
 	const { ref, setIsShow, isShow } = useOutside(false)
 
-    const handleToggleShow = () => {
-        setIsShow(!isShow)
-    }
+	const handleToggleShow = () => {
+		setIsShow(!isShow)
+	}
 
-	return <div className={styles.wrapper} ref={ref}>
-        {clickableElement(handleToggleShow)}
-        {isShow && children}
-    </div>
+	return (
+		<div className={styles.wrapper} ref={ref}>
+			{clickableElement(handleToggleShow)}
+			{
+				<motion.div
+					initial="closed"
+					animate={isShow ? 'open' : 'closed'}
+					variants={DROP_DOWN_LEFT_TO_RIGHT_ANIMATION}
+				>
+					{children}
+				</motion.div>
+			}
+		</div>
+	)
 }
 
 export default DropDownWrapper
