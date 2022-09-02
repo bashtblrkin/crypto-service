8d31884e90873e69a55d14cfd7f0b28fb65ea1d8
атурин ван <baturin.i@asbc.ru>
2022-09-02 18:24:11 +0300

    upd conf

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  poweredByHeader: false,
+  env: {
+    APP_URL: process.env.REACT_APP_URL,
+    APP_ENV: process.env.REACT_APP_ENV,
+    APP_SERVER_URL: process.env.REACT_APP_SERVER_URL
+  },
+  images: {
+    domains: ['localhost', 'cloudflare-ipfs.com']
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:4000/api/:path*'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
