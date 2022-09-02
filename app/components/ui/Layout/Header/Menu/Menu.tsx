078784c564f7318407075e5fe996f982dab1612c
атурин ван <baturin.i@asbc.ru>
2022-09-02 18:26:49 +0300

    add logout action

--- app/components/ui/Layout/Header/Menu/Menu.tsx
+++ app/components/ui/Layout/Header/Menu/Menu.tsx
@@ -4,21 +4,21 @@ import {CgProfile} from "react-icons/cg";
 import {MdLogout} from "react-icons/md";
 
 import styles from './Menu.module.scss'
+import {AuthService} from "@/services/auth/auth.service";
 
 const Menu = () => {
+
     return (
         <nav className={styles.nav}>
             <ul>
                 <Link href={'/profile'}>
                     <li>
-                        <CgProfile />Профиль
+                        <CgProfile/>Профиль
                     </li>
                 </Link>
-                <Link href={'/logout'}>
-                    <li>
-                        <MdLogout />Выйти
-                    </li>
-                </Link>
+                <li onClick={() => {AuthService.logout()}}>
+                    <MdLogout/>Выйти
+                </li>
             </ul>
         </nav>
     );
