c28ac075871dd406cfb419a2a4a50f1ac193c2f7
атурин ван <baturin.i@asbc.ru>
2022-09-02 18:29:32 +0300

    used token

--- app/providers/AuthProvider/AuthProvider.tsx
+++ app/providers/AuthProvider/AuthProvider.tsx
@@ -1,7 +1,10 @@
-import { IAuthContext, TypeUserState } from './auth.interface'
-import React, { FC, PropsWithChildren, createContext, useState } from 'react'
+import {IAuthContext, TypeUserState} from './auth.interface'
+import React, {createContext, FC, PropsWithChildren, useEffect, useState} from 'react'
 
-import { User } from '@/shared/interfaces/user.interface'
+import {User} from '@/shared/interfaces/user.interface'
+import {useRouter} from "next/router";
+import Cookies from "js-cookie";
+import {AuthService} from "@/services/auth/auth.service";
 
 export const AuthContext = createContext({} as IAuthContext)
 
@@ -13,7 +16,27 @@ export const AuthContext = createContext({} as IAuthContext)
 	}*/
 
 const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
+
 	const [user, setUser] = useState<TypeUserState>(null)
+
+	const {pathname} = useRouter()
+
+	useEffect(() => {
+		const accessToken = Cookies.get('accessToken')
+		if (!accessToken) return
+
+		const user = JSON.parse(localStorage.getItem('user') || '')
+
+		setUser(user)
+	}, [])
+
+	useEffect(() => {
+		const accessToken = Cookies.get('accessToken')
+		if (!accessToken && !user) {
+			AuthService.logout()
+			setUser(null)
+		}
+	}, [pathname]) //eslint-disable-line react-hooks/exhaustive-deps
 
 	const handleSetUser = (user: User) => {
 		setUser(user)
