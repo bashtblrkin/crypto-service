17cdfd707fd2f2c08e7ad968e7c4b209f704135c
атурин ван <baturin.i@asbc.ru>
2022-09-02 18:26:21 +0300

    del auth provider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 
 import Layout from '@/ui/Layout/Layout'
-import AuthProvider from "../app/providers/AuthProvider/AuthProvider";
 
 const Home: NextPage = () => {
-	return (
-		<AuthProvider>
-			<Layout title="Crypto DOM">
-				Главная
-			</Layout>
-		</AuthProvider>
-	)
+    return (
+        <Layout title="Crypto DOM">
+            Главная
+        </Layout>
+    )
 }
 
 export default Home
